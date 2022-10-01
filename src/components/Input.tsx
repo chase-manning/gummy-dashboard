9dcbbe20350e8ac67d372e252e6008a83e45c4de
chase-manning <chase@manning.dev>
2022-10-01 10:51:44 +0100

    :lipstick: add styling for input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,69 @@
 import styled from "styled-components";
 
-const StyledInput = styled.input`
-  display: flex;
+const Container = styled.div`
+  position: relative;
+  width: 100%;
 `;
 
-interface Props {
-  value: string;
-  setValue: (value: string) => void;
-}
+const StyledInput = styled.input`
+  border-radius: 1rem;
+  border: 2px solid var(--plain-shadow);
+  box-shadow: 0 0.2rem 0 0 var(--plain-shadow);
+  padding: 1rem 2rem;
+  font-size: 1.4rem;
+  font-weight: 600;
+  outline: none;
+  color: var(--main);
+  width: 100%;
 
-const Input = ({ value, setValue }: Props) => {
-  return <StyledInput>meow</StyledInput>;
+  ::placeholder {
+    color: var(--sub);
+  }
+`;
+
+const Max = styled.button`
+  font-size: 1rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  color: var(--sub);
+  cursor: pointer;
+  padding: 0.5rem;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80%;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 0.5rem;
+  border: none;
+  margin-top: 0;
+  background-color: var(--plain);
+  width: 100%;
+`;
+
+type Props = {
+  value: string;
+  update: (value: string) => void;
+  placeholder?: string;
+  type?: string;
+  max?: string;
+};
+
+const Input: React.FC<Props> = (props) => {
+  return (
+    <Container>
+      <StyledInput
+        value={props.value}
+        type={props.type || "text"}
+        placeholder={props.placeholder}
+        onChange={(event) => props.update(event.target.value)}
+      />
+      {props.max && (
+        <Max onClick={() => props.update(props.max || "0")}>max</Max>
+      )}
+    </Container>
+  );
 };
 
 export default Input;
