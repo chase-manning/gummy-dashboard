316cd59bbcb01a89ba536a9ae68a225b92284085
chase-manning <chase@manning.dev>
2022-10-01 10:36:39 +0100

    :lipstick: add global styling

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "./state/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -11,6 +12,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyles />
       <App />
     </Provider>
   </React.StrictMode>
