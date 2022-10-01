0b1f750cfa7a13d7a21e09d9af7544edad917eee
chase-manning <chase@manning.dev>
2022-10-01 10:52:17 +0100

    :rocket: add ability to add to dashboard

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
+import { addIframe } from "../state/coreSlice";
+import Input from "./Input";
 import Popup from "./Popup";
 
 const StyledCreate = styled.div`
   display: flex;
   width: 100%;
-  border: 1px solid var(--border);
 `;
 
 interface Props {
@@ -13,14 +16,32 @@ interface Props {
 }
 
 const Create = ({ show, close }: Props) => {
+  const [value, setValue] = useState("");
+
+  const dispatch = useDispatch();
+
   return (
     <Popup
       show={show}
       close={close}
-      action={() => console.log("meow")}
-      header="Enter url"
+      action={() =>
+        dispatch(
+          addIframe({
+            id: Math.round(Math.random() * 10000000000).toString(),
+            url: value,
+          })
+        )
+      }
+      header="Enter website url"
+      buttonText="Add to dashboard"
     >
-      <StyledCreate>meow</StyledCreate>
+      <StyledCreate>
+        <Input
+          value={value}
+          update={(value: string) => setValue(value)}
+          placeholder="https://chase.manning.dev/"
+        />
+      </StyledCreate>
     </Popup>
   );
 };
