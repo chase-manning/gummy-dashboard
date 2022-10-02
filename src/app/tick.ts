5efa22a269a7f54637c8729406328475b145b795
chase-manning <chase@manning.dev>
2022-10-02 12:20:06 +0100

    :sparkles: add tick hook

---
+++ src/app/tick.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const TICKS_PER_MINUTE = 1;
+
+const useTick = () => {
+  const [tick, setTick] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTick(tick + 1);
+    }, 60000 / TICKS_PER_MINUTE);
+    return () => clearInterval(interval);
+  }, [tick]);
+
+  return tick;
+};
+
+export default useTick;
